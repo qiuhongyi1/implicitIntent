ef98e18a928eb2dbc7a9bd968a9651ab158bd2a9
qiuhongyi1 <344880508@qq.com>
2017-09-14 09:22:06 +0800

    Initial commit

---
+++ app/src/main/java/com/example/qiuhongyi/implicitintent/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.qiuhongyi.implicitintent;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button btn=(Button)findViewById(R.id.buttonTest);
+        btn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse("http://www.baidu.com"));
+                startActivity(intent);
+            }
+        });
+    }
+}
